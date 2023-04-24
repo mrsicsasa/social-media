b9df8e3117070f78b9c3a19c79f0752fa907fceb
mrsicsasa <sasamrsic20@gmail.com>
2023-04-24 17:37:28 +0200

    Account Settings

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path('signup',views.signup, name="signup"),
     path('signin',views.signin, name="signun"),
     path('logout',views.logout, name="logout"),
+    path('settings',views.settings, name="settings"),
 ]
